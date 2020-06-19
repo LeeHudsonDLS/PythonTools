29b26c96db0b14c73c93e3b376ec1946e02439f8
Lee Hudson <lee.hudson@diamond.ac.uk>
2020-06-19 11:47:35 +0100

    Added script to set env vars for qtdesigner

---
+++ qtdesignerEnv.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=/dls_sw/prod/tools/RHEL6-x86_64/Python/2-7-3/prefix/lib
+export PYTHONPATH=/dls_sw/prod/common/python/RHEL7-x86_64/dls_pyqt4widgets/2-0/dls_pyqt4widgets:/dls_sw/prod/common/python/RHEL7-x86_64/dls_pyqt4widgets/2-0:/dls_sw/prod/R3.14.12.7/support/rga/4-21/data:/dls_sw/prod/R3.14.12.3/support/digitelMpc/5-13/data:/dls_sw/prod/R3.14.12.7/support/FEQt4/3-9-1/Python
+export PYQTDESIGNERPATH=/dls_sw/prod/common/python/RHEL7-x86_64/dls_pyqt4widgets/2-0/designerplugins:/dls_sw/prod/R3.14.12.7/support/rga/4-21/data:/dls_sw/prod/R3.14.12.3/support/digitelMpc/5-13/data
+export PYEPICS_LIBCA=/dls_sw/epics/R3.14.11/base/lib/linux-x86_64/libca.so
+export QT_GRAPHICSSYTEM=raster
