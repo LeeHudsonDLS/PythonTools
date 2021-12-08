e3add5c96aea0197bee77b93734b23838a4042d6
Lee Hudson <lee.hudson@diamond.ac.uk>
2021-12-08 11:44:41 +0000

    Added the ability to compare across cells

--- srTemplateChecker/srTemplateChecker.py
+++ srTemplateChecker/srTemplateChecker.py
@@ -1,7 +1,7 @@
 #!/bin/env dls-python3
 
 import sys
-import os
+import re
 from subprocess import Popen, PIPE
 import argparse
 
@@ -15,6 +15,9 @@ args=parser.parse_args()
 getIOCPathString = "configure-ioc list | grep ^SR[0-2][0-9]C-VA-IOC-01 | grep vxWorks"
 
 iocSubFileList = list()
+commonality = dict()
+fileInstanceList = dict()
+fileInstanceListRaw = dict()
 
 # Get list of substitution files
 for redirectorOp in Popen(getIOCPathString,shell=True,stdout=PIPE).stdout.read().decode().split('\n'):
@@ -33,11 +36,16 @@ for sub in iocSubFileList:
     fileInstance = ""
     openBracketCounter = 0
     closedBracketCounter = 0
+    prefix = ""
 
     for line in lines:
         if foundFile:
             if line[0] != '#':
                 fileInstance += line
+                if len(prefix) < 1:
+                    reResult = re.compile("SR[0-2][0-9]").findall(line)
+                    if len(reResult) > 0:
+                        prefix = reResult[0]
         if f"file {args.file}" in line and not foundFile:
             foundFile = True
             fileInstance += line
@@ -50,5 +58,31 @@ for sub in iocSubFileList:
             foundFile = False
             openBracketCounter = 0
             closedBracketCounter = 0
-    print(fileInstance)
+    
+    fileInstance = fileInstance.replace(prefix,"SRxx")
+    fileInstanceList[prefix]=fileInstance
+    fileInstanceListRaw[prefix]=fileInstance.replace(' ','').replace('\r','').replace('\n','').replace('\t','')
+
+# Create a dict as follows:
+# {<rawSubstitutionText>:<list of cells that have this exact text>} eg:
+# {rawSubstitutionText:[SR01,SR02,SR03]}
+for fi in fileInstanceListRaw:
+    fileInstanceText = fileInstanceListRaw[fi]
+    if fileInstanceText in commonality:
+        commonality[fileInstanceText].append(fi)
+    else:
+        commonality[fileInstanceText]=list()
+        commonality[fileInstanceText].append(fi)
+
+
+
+
+for config in commonality:
+    cells = ""
+    cell=commonality[config][0]
+    for cell in commonality[config]:
+        cells += f"{cell} "
+    cells += ':'
+    print(cells)
+    print(fileInstanceList[cell])
         
