4ea0ed04f55e21f9ae1822a1acadb73fa1c42737
Lee Hudson <lee.hudson@diamond.ac.uk>
2021-12-09 10:41:55 +0000

    Working on SR builder migration tool

---
+++ subsToXml/subsToXml.py
@@ -0,0 +1,164 @@
+#!/bin/env dls-python3
+
+import sys
+import re
+from subprocess import Popen, PIPE
+import argparse
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file",nargs='?', help="Path to substitution file", default="/dls_sw/work/R3.14.12.7/ioc/SR24C/VA/SR24C-VA-IOC-01App/Db/SR24C-VA-IOC-01.substitutions")
+args=parser.parse_args()
+
+needsNameField = ['mks937a','mks937aGauge','digitelMpc']
+asynPortDevice = ['mks937a','digitelMpc']
+portLookup = dict()
+
+
+# Returns string describing the a suitable name for the device
+def getName(instance,fileNameNoExt):
+    name = ''
+    if fileNameNoExt == 'mks937a':
+        device = instance.strip().split()[0]
+        name = f"GCTLR_{device[4]}_{device[-2:]}"
+    if fileNameNoExt == 'mks937aGauge':
+        dom = instance.strip().split()[0]
+        id = instance.strip().split()[1]
+        name = f"GAUGE_{dom[-1]}_{id}"
+    if fileNameNoExt == 'digitelMpc':
+        device = instance.strip().split()[0]
+        name = f"MPC_{device[4]}_{device[-2:]}"
+
+    return name
+
+def extractPattern(substitutionString,fileNameNoExt):
+
+    removeFromPattern = ["pattern","{","}",","]
+    patStart = substitutionString.find("pattern")
+    patEnd = substitutionString.find('}')+1
+
+    patternSubString  = substitutionString[patStart:patEnd]
+
+    for token in removeFromPattern:
+        patternSubString = patternSubString.replace(token,'')
+
+
+    pattern = patternSubString.split()
+    if fileNameNoExt in needsNameField:
+        pattern.insert(0,'name')
+
+    return pattern
+
+# Returns list of stings describing the field values separated by whitespace, eg:
+# result[0] = 'SR24S-VA-GCTLR-01   ty_40_0'
+# result[1] = 'SR24S-VA-GCTLR-01   ty_40_1'
+def extractInstancesIntoList(substitutionString,fileNameNoExt):
+
+
+    removeFromInstance = ["\n","\t","}",'"']
+
+    result = substitutionString.split('{')
+    result = result[3:]
+    for i, instance in enumerate(result):
+        for token in removeFromInstance:
+            instance = instance.replace(token,'')
+        instance = instance.replace(',',' ')
+        instance = instance.replace('&', "&amp;")
+        
+        # Gets name field if needed
+        name = getName(instance,fileNameNoExt)
+        result[i]=f"{name} {instance.strip()}"
+        
+    return result
+
+def getModuleName(templateName):
+    builderClassLookup = {'mks937a':["mks937a","mks937aGauge","mks937aImg","mks937aPirg","mks937aGaugeGroup","mks937aImgGroup","mks937aPirgGroup","mks937aImgMean"],
+                          'digitelMpc':["digitelMpc","digitelMpcIonp","digitelMpcTsp","digitelMpcIonpGroup","digitelMpcTspGroup"],
+                          'rga':["rga"],
+                          'vacuumSpace':["space"],
+                          'rackFan':["rackFan"],
+                          'Hy8401ip':["Hy8401ip"],
+                          'dlsPLC':["dlsPLC_read100","dlsPLC_vacValveDebounce","dlsPLC_vacValveGroup"]}
+
+    for module in builderClassLookup:
+        if templateName in builderClassLookup[module]:
+            return module
+
+    return ''
+
+def getClassName(fileNameNoExt):
+
+    autoClassList = ['Hy8401ip']
+    
+    if fileNameNoExt in autoClassList:
+        return f"auto_{fileNameNoExt}"
+    else:
+        return fileNameNoExt
+
+fileInstanceDict = dict()
+
+
+sub = args.file
+substitutionFile = open(sub,'r')
+lines = substitutionFile.readlines()
+foundFile = False
+
+fileInstance = ""
+openBracketCounter = 0
+closedBracketCounter = 0
+templateFileName = ""
+
+for line in lines:
+    if line[0] != '#':
+
+        if foundFile:
+                fileInstance += line
+
+        if "file" in line and not foundFile:
+            templateFileName = line.split()[-1]
+            foundFile = True
+            fileInstance += line
+
+        if foundFile:
+            if '{' in line:
+                openBracketCounter += 1
+            if '}' in line:
+                closedBracketCounter += 1
+
+        if openBracketCounter > 0 and openBracketCounter == closedBracketCounter:
+            fileInstanceDict[templateFileName]=fileInstance
+            foundFile = False
+            openBracketCounter = 0
+            closedBracketCounter = 0
+            fileInstance = ""
+
+
+def getPortElementNumber(instanceValues):
+    for i, instance in enumerate(instanceValues):
+        if 'ty_' in instance:
+            return i
+    return -1
+
+
+for fileInstance in fileInstanceDict:
+    template = fileInstanceDict[fileInstance]
+
+    fileName=fileInstance
+    fileNameNoExt=fileName.split('.')[0]
+
+    patternList = extractPattern(template,fileNameNoExt)
+    instList = extractInstancesIntoList(template,fileNameNoExt)
+
+    for instance in instList:
+        xmlString = f"<{getModuleName(fileNameNoExt)}.{getClassName(fileNameNoExt)} "
+        instanceValues = instance.split()
+
+        if fileNameNoExt in asynPortDevice:
+            portLookup[instanceValues[0]] = instanceValues[getPortElementNumber(instanceValues)]      
+
+        for i,p in zip(instanceValues,patternList):
+            xmlString += f'{p}="{i}" '
+        xmlString += "/>"
+        print(xmlString)
+print("here")
