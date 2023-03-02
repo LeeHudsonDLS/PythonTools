34d229d2aa32cffe2c45d4e3fc9ccf315789d6b9
Lee Hudson <lee.hudson@diamond.ac.uk>
2023-03-02 07:43:52 +0000

    LED board serial tester

---
+++ serialTest/test.py
@@ -0,0 +1,36 @@
+
+import serial, time
+#initialization and open the port
+
+#possible timeout values:
+#    1. None: wait forever, block call
+#    2. 0: non-blocking mode, return immediately
+#    3. x, x is bigger than 0, float allowed, timeout block call
+
+ser = serial.Serial("/dev/ttyUSB0")
+#ser.port = "/dev/ttyUSB7"
+#ser.port = "/dev/ttyS2"
+ser.baudrate = 9600
+ser.bytesize = serial.EIGHTBITS #number of bits per bytes
+ser.parity = serial.PARITY_NONE #set parity check: no parity
+ser.stopbits = serial.STOPBITS_ONE #number of stop bits
+#ser.timeout = None          #block read
+ser.timeout = 1            #non-block read
+#ser.timeout = 2              #timeout block read
+ser.xonxoff = False     #disable software flow control
+ser.rtscts = False     #disable hardware (RTS/CTS) flow control
+ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
+ser.writeTimeout = 2     #timeout for write
+
+
+#ser.open()
+
+
+val = 1
+while val <= 1048576:
+    cmd = f'{val},100\r\n'
+    ser.write(str.encode(cmd))
+    val = val*2
+    time.sleep(0.5)
+
+ser.close()
